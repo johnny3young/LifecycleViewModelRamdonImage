ec5d25bcd98ab93e4191147cfa699f25a895ecb3
johnny3young <youngerblack@gmail.com>
2019-07-25 11:46:16 -0500

    working 100% with the 2 images y change it when push button

--- app/src/main/java/com/black3/app/lifecycleviewmodelramdonimage/MainActivity.kt
+++ app/src/main/java/com/black3/app/lifecycleviewmodelramdonimage/MainActivity.kt
@@ -2,26 +2,44 @@ package com.black3.app.lifecycleviewmodelramdonimage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+import android.util.Log.e
+import android.widget.Button
 import android.widget.ImageView
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
 
+    //lateinit var imagePhoto1 : ImageView
+    //lateinit var imagePhoto2 : ImageView
+    //lateinit var btnGetUrlImage : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var imagePhoto : ImageView = findViewById(R.id.imagePhoto)
+        var imagePhoto2 : ImageView = findViewById(R.id.imagePhoto2)
+        var imagePhoto1 : ImageView = findViewById(R.id.imagePhoto1)
+        var btnGetUrlImage : Button = findViewById(R.id.btnGetUrlImage)
         //var model = MainActivityViewModel()
 
         //Extraemos el ViewModel a través del ViewModelProviders y le damos el nombre de la clase que se está dando de alta
         var model = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
 
-        Picasso.get().load(model.callUrlImage()).into(imagePhoto)
-        Log.e("Tag Picasso", "Putting the image in the imagePhoto")
+        var urlImage = model.callUrlImage()
+        urlImage?.observe(this, Observer {
+            print("Se ejecuta si la url es modificada")
+
+            Picasso.get().load(it).into(imagePhoto1)
+            Picasso.get().load(it).into(imagePhoto2)
+
+            e("Tag Picasso", "Putting the image in the imagePhoto")
+        })
+
+        btnGetUrlImage.setOnClickListener {
+            model.randomNumberUrl()
+        }
 
     }
 }
--- app/src/main/java/com/black3/app/lifecycleviewmodelramdonimage/MainActivityViewModel.kt
+++ app/src/main/java/com/black3/app/lifecycleviewmodelramdonimage/MainActivityViewModel.kt
@@ -1,23 +1,26 @@
 package com.black3.app.lifecycleviewmodelramdonimage
 
-import android.util.Log
+import android.util.Log.e
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 //Implementamos ViewModel de LifeCycle para poder mantener la misma imagen al rotar el celular
 class MainActivityViewModel : ViewModel() {
 
     //Esta clase tiene la responsabilidad de respoder lo que le pida la UI
-    private var urlImage : String? = null
+    //Para convertir el String en "observer" cambiamos private var urlImage : String? = null a private var urlImage : MutableLiveData<String?>? = null
+    private var urlImage : MutableLiveData<String?>? = null
 
-    fun callUrlImage() : String? {
+    fun callUrlImage() : MutableLiveData<String?>? {
         if (urlImage == null){
-            urlImage = randomNumberUrl()
+            urlImage = MutableLiveData()
+            randomNumberUrl()
         }
         return urlImage
     }
 
-    fun randomNumberUrl() : String {
-        return "https://picsum.photos/${(3..5).random()}00/${(3..5).random()}00"
-        Log.e("ramdomNumberUrl","Image random from the method randomNumberUrl")
+    fun randomNumberUrl() {
+        urlImage?.value = "https://picsum.photos/${(3..5).random()}00/${(3..5).random()}00"
+        e("ramdomNumberUrl","Image random from the method randomNumberUrl")
     }
 }
